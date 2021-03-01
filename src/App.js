9ba155cb337d5d690fb23dfff46fbad7ce1c196a
Igor <munkieryder@gmail.com>
2021-03-01 15:15:46 +0100

    Footer + SearchPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
 
-import Home from "./Home/Home";
-import Header from "./Header/Header";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import SearchPage from "./components/SearchPage/SearchPage";
+import Footer from "./components/Footer/Footer";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,21 @@
 import React from "react";
 
-import airbnb_logo from "../assets/logo-airbnb.png";
+import airbnb_logo from "../../assets/logo-airbnb.png";
 
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <img src={airbnb_logo} alt="Airbnb Logo" className="header__logo" />
+      <Link to="/">
+        <img src={airbnb_logo} alt="Airbnb Logo" className="header__logo" />
+      </Link>
 
       <div className="header__center">
         <input type="text" />
